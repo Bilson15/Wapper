e78eb4bf92d48d5f27470a2b79dacd540da43c0d
bilson15 <gabrielfelipe.bil@gmail.com>
2022-08-02 22:12:29 -0300

    continuação 02/08

--- lib/app/ui/service_parceiro/view/service_parceiro.dart
+++ lib/app/ui/service_parceiro/view/service_parceiro.dart
@@ -70,12 +70,115 @@ class ServicoParceiro extends StatelessWidget {
                   ),
                 ],
               ),
-
-              // CardServiceSeller(
-              //   title: 'Corte de cabelo + Barba',
-              //   description: 'Corte mascúlino, degradê, social ou á sua escolha',
-              //   value: 'R\$ 25,00',
-              // ),
+              GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext bc) {
+                        return Container(
+                          height: Get.height / 2,
+                          child: ListView.builder(
+                            itemCount: 10,
+                            itemBuilder: ((context, index) {
+                              return ListTile(leading: new Icon(Icons.music_note), title: new Text('Músicas'), onTap: () => {});
+                            }),
+                          ),
+                        );
+                      });
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 60,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(80), color: backgroundFieldColor),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 8),
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: NetworkImage(
+                                        "https://img.elo7.com.br/product/zoom/2E9706C/logotipo-personalizada-barbearia-arte-digital-tesoura.jpg"),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 8),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          TextComponent(
+                                            'Alexandre',
+                                            color: fontColor,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            size: 15,
+                                            Icons.star,
+                                            color: amareloPadrao,
+                                          ),
+                                          const SizedBox(
+                                            width: 4,
+                                          ),
+                                          TextComponent(
+                                            '4,5',
+                                            color: amareloPadrao,
+                                            fontSize: 13,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          const SizedBox(
+                                            width: 4,
+                                          ),
+                                          Container(
+                                            child: TextComponent(
+                                              '• Barbeiro • Mascúlino',
+                                              color: cinzaPadrao,
+                                              textOverflow: TextOverflow.ellipsis,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 12,
+                                            ),
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  )),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    size: 40,
+                                    Icons.keyboard_arrow_down,
+                                    color: fontColor,
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),

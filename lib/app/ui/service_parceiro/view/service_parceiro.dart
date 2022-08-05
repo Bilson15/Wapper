9a8e2c4767ddb19132438789bc39a17d7724b840
bilson15 <gabrielfelipe.bil@gmail.com>
2022-08-05 20:50:56 -0300

    continuação service seller

--- lib/app/ui/service_parceiro/view/service_parceiro.dart
+++ lib/app/ui/service_parceiro/view/service_parceiro.dart
@@ -61,7 +61,7 @@ class ServicoParceiro extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: EdgeInsets.symmetric(vertical: 16),
+                    padding: EdgeInsets.only(top: 4, bottom: 16),
                     child: TextComponent(
                       'Selecione o Profissional',
                       fontWeight: FontWeight.w600,
@@ -74,15 +74,87 @@ class ServicoParceiro extends StatelessWidget {
                 onTap: () {
                   showModalBottomSheet(
                       context: context,
-                      builder: (BuildContext bc) {
-                        return Container(
-                          height: Get.height / 2,
-                          child: ListView.builder(
-                            itemCount: 10,
-                            itemBuilder: ((context, index) {
-                              return ListTile(leading: new Icon(Icons.music_note), title: new Text('Músicas'), onTap: () => {});
-                            }),
-                          ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(18), topRight: Radius.circular(18)),
+                      ),
+                      elevation: 0,
+                      builder: (BuildContext context) {
+                        return Stack(
+                          children: [
+                            Container(
+                              height: Get.height / 2,
+                              child: ListView.builder(
+                                itemCount: 10,
+                                itemBuilder: ((context, index) {
+                                  return ListTile(
+                                      leading: Container(
+                                        width: 50,
+                                        height: 50,
+                                        decoration: const BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          image: DecorationImage(
+                                            fit: BoxFit.fill,
+                                            image: NetworkImage(
+                                                "https://media.istockphoto.com/illustrations/illustration-of-a-stylish-young-man-avatar-of-a-man-for-profile-for-illustration-id1256010006"),
+                                          ),
+                                        ),
+                                      ),
+                                      title: new Text('Alexandre ${index}'),
+                                      subtitle: Row(
+                                        children: [
+                                          Row(
+                                            children: [
+                                              Icon(
+                                                size: 15,
+                                                Icons.star,
+                                                color: amareloPadrao,
+                                              ),
+                                              const SizedBox(
+                                                width: 4,
+                                              ),
+                                              TextComponent(
+                                                '4,5',
+                                                color: amareloPadrao,
+                                                fontSize: 13,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                              const SizedBox(
+                                                width: 4,
+                                              ),
+                                              Container(
+                                                child: TextComponent(
+                                                  '• Barbeiro • Mascúlino',
+                                                  color: cinzaPadrao,
+                                                  textOverflow: TextOverflow.ellipsis,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 12,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                      onTap: () => {});
+                                }),
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.only(top: 10),
+                                  height: 4,
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                    color: cinzaPadrao,
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(20),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         );
                       });
                 },
@@ -105,7 +177,7 @@ class ServicoParceiro extends StatelessWidget {
                                   image: DecorationImage(
                                     fit: BoxFit.fill,
                                     image: NetworkImage(
-                                        "https://img.elo7.com.br/product/zoom/2E9706C/logotipo-personalizada-barbearia-arte-digital-tesoura.jpg"),
+                                        "https://media.istockphoto.com/illustrations/illustration-of-a-stylish-young-man-avatar-of-a-man-for-profile-for-illustration-id1256010006"),
                                   ),
                                 ),
                               ),
@@ -178,7 +250,220 @@ class ServicoParceiro extends StatelessWidget {
                     ),
                   ],
                 ),
-              )
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Column(
+                children: [
+                  Divider(
+                    thickness: 1,
+                    color: lineColor,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 4, bottom: 16),
+                        child: TextComponent(
+                          'Selecione o Horário',
+                          fontWeight: FontWeight.w600,
+                          color: fontColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet(
+                          context: context,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(18), topRight: Radius.circular(18)),
+                          ),
+                          elevation: 0,
+                          builder: (BuildContext context) {
+                            return Stack(
+                              children: [
+                                Container(
+                                  height: Get.height / 2,
+                                  child: ListView.builder(
+                                    itemCount: 10,
+                                    itemBuilder: ((context, index) {
+                                      return ListTile(
+                                          title: Row(
+                                            children: [
+                                              Padding(
+                                                padding: EdgeInsets.symmetric(horizontal: 4),
+                                                child: TextComponent(
+                                                  '12:00',
+                                                  color: cinzaPadrao,
+                                                  textOverflow: TextOverflow.ellipsis,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 12,
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding: EdgeInsets.symmetric(horizontal: 4),
+                                                child: TextComponent(
+                                                  '•',
+                                                  color: cinzaPadrao,
+                                                  textOverflow: TextOverflow.ellipsis,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 12,
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding: EdgeInsets.symmetric(horizontal: 4),
+                                                child: TextComponent(
+                                                  '13:00',
+                                                  color: cinzaPadrao,
+                                                  textOverflow: TextOverflow.ellipsis,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 12,
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding: EdgeInsets.symmetric(horizontal: 4),
+                                                child: TextComponent(
+                                                  '-',
+                                                  color: cinzaPadrao,
+                                                  textOverflow: TextOverflow.ellipsis,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 12,
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding: EdgeInsets.symmetric(horizontal: 4),
+                                                child: TextComponent(
+                                                  (index % 2 == 0 ? 'Disponível' : 'Reservado'),
+                                                  color: cinzaPadrao,
+                                                  textOverflow: TextOverflow.ellipsis,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 12,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          onTap: () => {});
+                                    }),
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(top: 10),
+                                      height: 4,
+                                      width: 70,
+                                      decoration: BoxDecoration(
+                                        color: cinzaPadrao,
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(20),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            );
+                          });
+                    },
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(80), color: backgroundFieldColor),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                      padding: EdgeInsets.symmetric(horizontal: 8),
+                                      child: Row(
+                                        children: [
+                                          Padding(
+                                            padding: EdgeInsets.symmetric(horizontal: 4),
+                                            child: TextComponent(
+                                              '12:00',
+                                              color: cinzaPadrao,
+                                              textOverflow: TextOverflow.ellipsis,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 12,
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: EdgeInsets.symmetric(horizontal: 4),
+                                            child: TextComponent(
+                                              '•',
+                                              color: cinzaPadrao,
+                                              textOverflow: TextOverflow.ellipsis,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 12,
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: EdgeInsets.symmetric(horizontal: 4),
+                                            child: TextComponent(
+                                              '13:00',
+                                              color: cinzaPadrao,
+                                              textOverflow: TextOverflow.ellipsis,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 12,
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: EdgeInsets.symmetric(horizontal: 4),
+                                            child: TextComponent(
+                                              '-',
+                                              color: cinzaPadrao,
+                                              textOverflow: TextOverflow.ellipsis,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 12,
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: EdgeInsets.symmetric(horizontal: 4),
+                                            child: TextComponent(
+                                              'Disponível',
+                                              color: cinzaPadrao,
+                                              textOverflow: TextOverflow.ellipsis,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 12,
+                                            ),
+                                          ),
+                                        ],
+                                      )),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        size: 40,
+                                        Icons.keyboard_arrow_down,
+                                        color: fontColor,
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Divider(
+                    thickness: 1,
+                    color: lineColor,
+                  ),
+                ],
+              ),
             ],
           ),
         ),
